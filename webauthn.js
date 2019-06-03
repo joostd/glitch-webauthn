28b0457c414e4e55f4b523433872d84630248d79
Joost van Dijk <vandijk.joost@gmail.com>
2019-06-03 16:30:53 +0200

    add login skeleton

--- webauthn.js
+++ webauthn.js
@@ -1,7 +1,12 @@
+PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable()
+  .then(function(available){ 
+    console.log("Verifying Platform Authenticator", available ? "is" : "is not", "available");
+  }).catch(console.error);
+
 var challenge = window.crypto.getRandomValues(new Uint8Array(8));
 var id = Uint8Array.from("1234");
 
-var publicKey = {
+var createOpts = {
     'challenge': challenge,
     'rp': {
         'name': 'Acme'
@@ -16,18 +21,44 @@ var publicKey = {
     ]
 }
 
-console.log(publicKey);
-console.log(JSON.stringify(publicKey));
+var getOpts = {
+    'challenge': challenge,
+    'rp': {
+        'name': 'Acme'
+    },
+    'user': {
+        'id': id,
+        'name': 'john',
+        'displayName': 'John'
+    },
+    'pubKeyCredParams': [
+        { 'type': 'public-key', 'alg': -7  },
+    ]
+}
 
-var result;
+console.log(createOpts);
 
 function register() {
-  navigator.credentials.create({ 'publicKey': publicKey })
-    .then(process)
+  navigator.credentials.create({ 'publicKey': createOpts })
+    .then(processCreate)
     .catch(console.log)
 }
 
-function process(cred) {
+function login() {
+  navigator.credentials.get({ 'publicKey': getOpts })
+    .then(processGet)
+    .catch(console.log)
+}
+
+function processCreate(cred) {
+  console.log(cred)
+  var enc = new TextDecoder(); // always utf-8
+  var clientData = enc.decode(cred.response.clientDataJSON);
+  console.log(clientData)
+  console.log(clientData.challenge);
+}
+
+function processGet(cred) {
   console.log(cred)
   var enc = new TextDecoder(); // always utf-8
   var clientData = enc.decode(cred.response.clientDataJSON);
